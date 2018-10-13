7733967c145c46eebb82737b6377162cb9c3d5fb
Dil3mm4 <dil3mm4.dev@gmail.com>
2018-10-13 00:29:28 +0000

    Let's try to speed up some stuff

--- board-base.mk
+++ board-base.mk
@@ -1,4 +1,6 @@
+ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.image-dex2oat-filter=verify-none
 TARGET_SYSTEM_PROP := device/phh/treble/system.prop $(TARGET_SYSTEM_PROP)
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/phh/treble/bluetooth
 TARGET_EXFAT_DRIVER := exfat
 DEVICE_FRAMEWORK_MANIFEST_FILE := device/phh/treble/framework_manifest.xml
+WITH_DEXPREOPT := true
